90bc8f80e6b7c6f1a81a637f58abaf7991a20c0f
Ian Holstead <ian.holstead@gmail.com>
2016-01-29 21:53:46 -0500

    Added Orb stuff, colour walls work

---
+++ GGJ16/Assets/Scripts/Orb.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Orb : MonoBehaviour {
+    public Color colour;
+
+    float initialHeight;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        hover();
+	}
+
+    void hover() {
+
+    }
+}
