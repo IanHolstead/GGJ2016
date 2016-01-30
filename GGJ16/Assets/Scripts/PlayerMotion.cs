d4c4e8f268f54c56e8dbce9078c0a54b1d99198f
Sebastian Malton <sebastian@malton.name>
2016-01-30 13:23:56 -0500

    Added Player Prefab

--- GGJ16/Assets/Scripts/PlayerMotion.cs
+++ GGJ16/Assets/Scripts/PlayerMotion.cs
@@ -10,9 +10,10 @@ public class PlayerMotion : MonoBehaviour
 	private string playerCode = "P";
 	private Vector3 rotation;
 	private float vertVel;
+	private bool isHoldingJump = false;
 
-    public bool hasDoubleJump = false;
-    public bool hasFastMovement = false;
+	public bool hasDoubleJump = false;
+	public bool hasFastMovement = false;
 
 	// Use this for initialization
 	void Start ()
@@ -26,42 +27,80 @@ public class PlayerMotion : MonoBehaviour
 		vertVel = GetComponent<Rigidbody2D> ().velocity.y;
 		Movement ();
 		Debug.Log (jumpsLeft);
+		if (playerID == 0 && Input.GetKeyDown (KeyCode.Joystick1Button0)) {
+			isHoldingJump = true;
+		} else if (playerID == 1 && Input.GetKeyDown (KeyCode.Joystick2Button0)) {
+			isHoldingJump = true;
+		} else if (playerID == 2 && Input.GetKeyDown (KeyCode.Joystick3Button0)) {
+			isHoldingJump = true;
+		} else if (playerID == 3 && Input.GetKeyDown (KeyCode.Joystick4Button0)) {
+			isHoldingJump = true;
+		} else if (playerID == 0 && Input.GetKeyUp (KeyCode.Joystick1Button0)) {
+			isHoldingJump = false;
+		} else if (playerID == 1 && Input.GetKeyUp (KeyCode.Joystick2Button0)) {
+			isHoldingJump = false;
+		} else if (playerID == 2 && Input.GetKeyUp (KeyCode.Joystick3Button0)) {
+			isHoldingJump = false;
+		} else if (playerID == 3 && Input.GetKeyUp (KeyCode.Joystick4Button0)) {
+			isHoldingJump = false;
+		}
 	}
 
 	void Movement ()
 	{
+		float jumpHeight = 300f;
+		bool jumping = false;
 		float hor = Input.GetAxis (playerCode + "Horizontal") * 50;
 		float jump = 0f;
-		if (isGrounded ^ isPlayered || jumpsLeft > 0 ) {
+		if (isGrounded ^ isPlayered) {
 			if (playerID == 0 && Input.GetKey (KeyCode.Joystick1Button0)) {
-				jump = 500f;
-				isGrounded = false;
-				jumpsLeft = jumpsLeft > 0 ? jumpsLeft - 1 : 0;
+				jumping = true;
 			} else if (playerID == 1 && Input.GetKey (KeyCode.Joystick2Button0)) {
-				jump = 500f;
-				isGrounded = false;
-				jumpsLeft = jumpsLeft > 0 ? jumpsLeft - 1 : 0;
+				jumping = true;
 			} else if (playerID == 2 && Input.GetKey (KeyCode.Joystick3Button0)) {
-				jump = 500f;
-				isGrounded = false;
-				jumpsLeft = jumpsLeft > 0 ? jumpsLeft - 1 : 0;
+				jumping = true;
 			} else if (playerID == 3 && Input.GetKey (KeyCode.Joystick4Button0)) {
-				jump = 500f;
+				jumping = true;
+			}
+			if (jumping) {
+				Debug.Log ("I am jumping");
+				jump = jumpHeight;
 				isGrounded = false;
 				jumpsLeft = jumpsLeft > 0 ? jumpsLeft - 1 : 0;
 			}
 		} else {
 			hor = hor / flyingSpeedDenominator;
 		}
+		if (!isGrounded && !isPlayered && !isHoldingJump && jumpsLeft > 0) {
+			Debug.Log ("I am able to DJump");
+			jumping = isJump ();
+			if (jumping) {
+				jump = jumpHeight;
+				isGrounded = false;
+				jumpsLeft = jumpsLeft > 0 ? jumpsLeft - 1 : 0;
+			}
+		}
 		GetComponent<Rigidbody2D> ().velocity += (new Vector2 (hor, 0) * Time.deltaTime);
 		GetComponent<Rigidbody2D> ().velocity += (new Vector2 (0, jump) * Time.deltaTime);
+	}
+
+	void isJump(){
+		if (playerID == 0 && Input.GetKey (KeyCode.Joystick1Button0)) {
+			return true;
+		} else if (playerID == 1 && Input.GetKey (KeyCode.Joystick2Button0)) {
+			return true;
+		} else if (playerID == 2 && Input.GetKey (KeyCode.Joystick3Button0)) {
+			return true;
+		} else if (playerID == 3 && Input.GetKey (KeyCode.Joystick4Button0)) {
+			return true;
+		}
 	}
 
 	void OnCollisionEnter2D (Collision2D c)
 	{
 		if (c.collider.sharedMaterial.name == "Ground") {
 			isGrounded = true;
-			jumpsLeft = jumpsLeft <= 2 ? jumpsLeft + 1 : 2;
+			jumpsLeft = jumpsLeft < 2 ? jumpsLeft + 1 : 2;
 		}
 		if (c.collider.sharedMaterial.name == "Player") {
 			isPlayered = true;
