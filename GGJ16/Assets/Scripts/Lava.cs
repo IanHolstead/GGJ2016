dbd14cc7c698f0ed5d01145633ef363eae8df7da
Derr1 <derrick.pollock@gmail.com>
2016-01-30 14:13:28 -0500

    Added Respawn Delay, Started Checkpoint Sytem

--- GGJ16/Assets/Scripts/Lava.cs
+++ GGJ16/Assets/Scripts/Lava.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class Lava : MonoBehaviour {
 
-    public Vector3 respawn = new Vector3(-3, 1, 0);
-
 	// Use this for initialization
 	void Start () {
 	    
@@ -18,10 +16,9 @@ public class Lava : MonoBehaviour {
     void OnTriggerEnter2D(Collider2D otherObj)
     {
         Debug.Log("Lava Collision!");
-        if(otherObj.gameObject.tag == "Player")
+        if (otherObj.gameObject.tag == "Player")
         {
-            otherObj.GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
-            otherObj.transform.position = respawn;
+            otherObj.GetComponent<SpriteRenderer>().enabled = false;
         }
     }
 
--- GGJ16/Assets/Scripts/testMovement.cs
+++ GGJ16/Assets/Scripts/testMovement.cs
@@ -5,9 +5,12 @@ public class testMovement : MonoBehaviour {
 
     public bool moveRight = true;
     public bool moveRightRigidBody = true;
+    public Vector3 respawn = new Vector3(-3, 1, 0);
+    public float respawnDelay = 1.0f;
+    private float respawnTime = 0.0f;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         if (moveRight)
         {
@@ -17,6 +20,17 @@ public class testMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if (GetComponent<SpriteRenderer>().enabled == false)
+        {
+            respawnTime += Time.deltaTime;
+            if (respawnTime >= respawnDelay)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector3(0, 0, 0);
+                transform.position = respawn;
+                GetComponent<SpriteRenderer>().enabled = true;
+                respawnTime = 0.0f;
+            }
+        }
         if (moveRightRigidBody)
         {
             transform.position = transform.position + new Vector3(0.1f, 0, 0);
