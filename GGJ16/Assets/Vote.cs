d160d6d3d56cbf4df2965e6effc02f7397601518
Jason Mittertreiner <jmittert@uwaterloo.ca>
2016-01-30 12:41:58 -0500

    Voting is mostly working

--- GGJ16/Assets/Vote.cs
+++ GGJ16/Assets/Vote.cs
@@ -1,46 +1,102 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class Vote : MonoBehaviour
 {
-	private Transform[] boxes = new Transform[4];
+	// List of lists for tracking the vote objects
+	private List<List<Transform>> playerVotes = new List<List<Transform>>();
+	private List<Transform> p1Votes = new List<Transform>();
+	private List<Transform> p2Votes = new List<Transform>();
+	private List<Transform> p3Votes = new List<Transform>();
+	private List<Transform> p4Votes = new List<Transform>();
+	public Text timerText;
 
-	int p1vote = -1;
+	float timer = 10;
 
 	// Use this for initialization
 	void Start ()
 	{
-	 	boxes[0] = gameObject.transform.FindChild("P1Box");
-	 	boxes[1] = gameObject.transform.FindChild("P2Box");
-	 	boxes[2] = gameObject.transform.FindChild("P3Box");
-	 	boxes[3] = gameObject.transform.FindChild("P4Box");
-		boxes[0].GetComponent<SpriteRenderer> ().enabled = false;
-		boxes[1].GetComponent<SpriteRenderer> ().enabled = false;
-		boxes[2].GetComponent<SpriteRenderer> ().enabled = false;
-		boxes[3].GetComponent<SpriteRenderer> ().enabled = false;
+		Debug.Log ("Start!");
+		playerVotes.Add (p1Votes);
+		playerVotes.Add (p2Votes);
+		playerVotes.Add (p3Votes);
+		playerVotes.Add (p4Votes);
+		timerText.text = timer.ToString();
+
+		for (int i = 0; i < 4; ++i) {
+			for (int ii = 0; ii < 4; ++ii) {
+				playerVotes[i].Add(gameObject.transform.FindChild("P"+ (i+1) + "Vote" + (ii+1)));
+				playerVotes[i][ii].GetComponent<SpriteRenderer> ().enabled = false;
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		Debug.Log ("Update!");
-		if (Input.GetAxis("GamePadVertical") > 0) {
-			p1vote = 0;
-		}
-		if (Input.GetAxis("GamePadHorizontal") > 0) {
-			p1vote = 1;
-		}
-		if (Input.GetAxis("GamePadVertical") < 0) {
-			p1vote = 2;
-		}
-		if (Input.GetAxis("GamePadHorizontal") < 0) {
-			p1vote = 3;
-		}
-		if (p1vote != -1) {
-			for (int i = 0; i < 4; i++) {
-				boxes[i].GetComponent<SpriteRenderer> ().enabled = false;
+
+		int[] playerBallets = new int[4] { 0, 0, 0, 0 };
+
+		// Collect the vote of each player
+		/*
+		for (int i = 0; i < 4; i++) {
+			if (Input.GetAxis("GamePad" + i + "Vertical") > 0) {
+				playerBallets[0]++;
 			}
-			boxes[p1vote].GetComponent<SpriteRenderer> ().enabled = true;
+			else if (Input.GetAxis("GamePad" + i + "Horizontal") > 0) {
+				playerBallets[1]++;
+			}
+			else if (Input.GetAxis("GamePad" + i + "Vertical") < 0) {
+				playerBallets[2]++;
+			}
+			else if (Input.GetAxis("GamePad" + i + "Horizontal") < 0) {
+				playerBallets[3]++;
+			}
+		}
+*/
+
+		// Render the appropriate vote boxes
+		for (int i = 0; i < 4; i++) {
+			for (int ii = 0; ii < 4; ii++) {
+				playerVotes[i][ii].GetComponent<SpriteRenderer> ().enabled = (playerBallets[i] > ii);
+			}
+		}
+
+		timer -= Time.deltaTime;
+		timerText.text = timer.ToString();
+		if (timer < 0) {
+			// Close the vote and call lightning
+			this.GetComponent<Renderer> ().enabled = false;
+			timerText.text = "";
+
+			// Figure out the character with the most votes
+			// Iterate through to find the most votes
+			int[] votesDist = new int[4] { 0, 0, 0, 0 };
+			for (int i = 0; i < 4; ++i) {
+				votesDist [playerBallets [i]]++;
+			}
+			// Iterate backwards through the vote distribution to
+			// find the player(s) with the most votes
+			for (int i = 3; i >= 0; --i) {
+				if (votesDist[i] == 1) {
+					// Iterate through to find the player with 
+					// those votes and zap them
+					for (int ii = 0; ii < 4; ++ii) {
+						if (playerBallets [ii] == i) {
+							Debug.Log ("Zapping Player" + ii);
+							//Zap(playerii)
+							return;
+						}
+					}
+				} else if (votesDist[i] >= 2 || i == 0) {
+					// If there is to "winners" or we've got to the 
+					//end of the array
+					Debug.Log ("Zapping Everyone");
+					//ZapAll()
+					return;
+				}
+			}
 		}
 	}
 }
